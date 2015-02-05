5e49a14952c63ba31c40424cde459f1ee16b8d95
MohamedBassem <medoox240@gmail.com>
2015-02-05 14:43:17 +0200

    Changing log path

--- Web/run.sh
+++ Web/run.sh
@@ -7,6 +7,8 @@ export SES_USER=""
 export SES_PASS=""
 export SES_PORT="25"
 
+mkdir -p /var/log/cabot
+
 service nginx restart &&\
-gunicorn cabot.wsgi:application --config gunicorn.conf --log-level info --log-file /var/log/gunicorn &\
+gunicorn cabot.wsgi:application --config gunicorn.conf --log-level info --log-file /var/log/cabot/gunicorn &\
 celery worker -B -A cabot --loglevel=INFO --concurrency=16 -Ofair
